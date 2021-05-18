8dde81cbcf14ccd35920ee08268f2a70e2540775
marialaina <pmarialaina@gmail.com>
2021-05-17 18:28:48 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// IMPORTING COMPENENTS
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Route, Switch } from "react-router-dom";
+// IMPORTING PAGES
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+
+const App = () => {
+    const URL = "https://full-stack-portfolio-mp.herokuapp.com/";
+
+    return (
+        <div className="App">
+            <Header />
+            <Switch>
+                <Route exact path="/">
+            <Home />
+            </Route>
+                <Route path="/projects">
+                <Projects URL={URL} />
+            </Route>
+            <Route path="/about">
+                <About URL={URL} />
+            </Route>
+            </Switch>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
