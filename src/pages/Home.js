b3ddc8bc50d7afe16114a93ab98af04c8b170e10
marialaina <pmarialaina@gmail.com>
2021-08-01 20:17:46 -0600

    removed a paragraph

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ const Home = (props) => {
     <h3>HI 👋🏽</h3>
     <h4>I'm Marialaina</h4>
     <p> I’m a software engineer with a passion for building and creating new things, with a strong inquisition about the way things work.  Took an interest in building  websites using Ruby On Rails;  by building websites,  I found a passion in writing code. I started off constructing buildings, and took a lot of pride in helping build something for people. Now that I have the opportunity, I’ve been exercising my love for creating new projects through software development.</p>
-    <h5> I love my dog Charlie and I like to code.</h5>
     <h4>The Skills:</h4>
     <li>Ruby</li>
     <li>Rails</li>
